06c76bea5f21850c604cf51c0b7eda5da2f5de76
kaotzi <tjgeorge@miners.utep.edu>
2018-05-14 15:59:57 -0600

    Add files via upload

---
+++ Thomas/tree/StartBlock.py
@@ -0,0 +1,10 @@
+from Block import *
+
+class StartBlock(Block):
+
+    def __init__(self, name, req, desc):
+        Block.__init__(self,name, req)
+        self.description = desc
+
+    def getDescript(self):
+        return self.description
