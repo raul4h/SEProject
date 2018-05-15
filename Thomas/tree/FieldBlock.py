937543fdbf6de444f4eac5b87d0b51f9bf24329e
kaotzi <tjgeorge@miners.utep.edu>
2018-05-14 18:15:06 -0600

    Add files via upload

---
+++ Thomas/tree/FieldBlock.py
@@ -0,0 +1,79 @@
+from Block import *
+
+class FieldBlock(Block):
+
+    def __init__(self, name, req):
+        Block.__init__(self,name, req)
+        self.isEnd = True
+		
+    def __init__(self, name, req, inf, ab, fdesc, dataT, base2, masck,cons,  siz):
+        Block.__init__(self,name, req)
+        self.isEnd = False
+        setInfo(self, inf)
+        setAbbr(self, ab)
+        setDesc(self, fdesc)
+        setType(self, dataT)
+        setBase(self, base2)
+        setMask(self, masck)
+        setConstraint(self, cons)
+        setSize(self, siz)
+		
+	
+		
+
+    def setInfo(self, information):
+        self.info = information
+		
+    def setAbbr(self, abbrev):
+        self.abbr = abbrev
+		
+    def setDesc(self, descrip):
+        self.desc = descrip
+		
+    def setType(self, type1):
+        self.types = type1
+			
+    def setBase(self, base1):
+        self.base = base1
+		
+    def setMask(self, masks):
+        self.mask=masks
+    
+    def setConstraint(self, constaints):
+        self.Constraint = constraints
+		
+    def setSize(self, sizes):
+        self.size= sizes	
+
+    def setIsEnd(self, end):
+        self.isEnd=end	
+	
+	
+    def getInfo(self):
+        return self.info
+		
+    def getAbbr(self):
+        return self.abbr
+		
+    def getDesc(self):
+        return self.desc
+		
+    def getType(self):
+        return self.types
+		
+    def getBase(self):
+        return self.base
+		
+    def getMask(self):
+        return self.mask
+    
+    def getConstraint(self):
+        return self.Constraint
+		
+    def getSize(self):
+        return self.size	
+
+    def getIsEnd(self):
+        return self.isEnd
+
+    	
