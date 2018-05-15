246a6d0dc025bd0b1c6bd9513dd89f6d5e82fe77
Unknown <rjhinostrozagarcia@miners.utep.edu>
2018-05-15 00:39:33 -0600

    Freddy

---
+++ BASELINE/Backend/Project.py
@@ -0,0 +1,9 @@
+import gc
+from Project_State import*
+from Storage import*
+
+
+
+
+list = ("Name", "Reuired", "Info","Abbreviation", "Description", "Data" , "base", None, "Constraint", 8)
+state = Project_State("Field", True, list)
